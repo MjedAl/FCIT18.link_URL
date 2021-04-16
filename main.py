ac6cf115d37117eaca647cb8b650e5d9425b3843
MjedAl <44271342+MjedAl@users.noreply.github.com>
2021-04-17 01:21:05 +0300

    login implemented

--- main.py
+++ main.py
@@ -1,49 +1,88 @@
 # Imports
 import json
 from flask import Flask, request, render_template, jsonify, abort, redirect, render_template, url_for, flash
-from functools import wraps
-from db import setup_db, db_drop_and_create_all,  User, Url, Subdomains
-# from db import User, Url
-from sqlalchemy import desc
+from db import setup_db, db_drop_and_create_all, User, Url, Subdomains
 import requests
 from flask_login import (
     LoginManager,
     current_user,
-    login_required,
     login_user,
-    logout_user,
 )
 import os
-from sqlalchemy.sql import func
 from flask_admin.contrib.sqla import ModelView
-from flask_admin import Admin
-import itertools
-
-
+from flask_admin import Admin, AdminIndexView
+from secret import captchaPrivateKey, flask_secret_key
 class myModelView(ModelView):
     def is_accessible(self):
         if current_user.is_authenticated:
             if current_user.role == 'admin':
                 return True
-        # TODO: Change to false after implementing login page
-        return True 
+        return False 
     def inaccessible_callback(self, name, **kwargs):
-        # TODO: redirect to login page
         return redirect(url_for('index'))
+
+class AdminIndex(AdminIndexView):
+    def is_accessible(self):
+        if current_user.is_authenticated:
+            if current_user.role == 'admin':
+                return True
+        return False 
+    def inaccessible_callback(self, name, **kwargs):
+        return redirect(url_for('index'))
+
 
 # App configuration
 app = Flask(__name__)
-app.secret_key = os.environ.get("SECRET_KEY") or os.urandom(24)
+app.secret_key = os.environ.get("SECRET_KEY") or flask_secret_key or os.urandom(24)
 login_manager = LoginManager()
 login_manager.init_app(app)
-admin = Admin(app, name='URLs CONTROL', template_mode='bootstrap3')
+admin = Admin(app, name='URLs CONTROL', template_mode='bootstrap3', index_view=AdminIndex())
 setup_db(app, admin, myModelView)
+captchaPrivateKey = os.getenv('captchaPrivateKey') or captchaPrivateKey
 
 @login_manager.user_loader
 def load_user(user_id):
     if user_id is None:
         return None
     return User.query.get(int(user_id))
+
+@app.route("/login", methods=['POST','GET'])
+def login():
+    if request.method == "POST":
+        token = request.form['token']
+        checkToken = requests.get('https://www.google.com/recaptcha/api/siteverify?secret='+captchaPrivateKey+'&response='+token+'&remoteip='+request.remote_addr)
+        if checkToken.json()['success']:
+            email = request.form["email"]
+            password = request.form["password"]
+            if email is not None and password is not None:
+                user = User.getByEmailAndPassword(email, password)
+                if user is None:
+                    return jsonify({
+                        'success': False,
+                        'message': 'Wrong email or password!'
+                    })
+                else:
+                    login_user(user, remember=True)
+                    return jsonify({
+                        'success': True
+                    })
+                    return redirect('/admin/', code=302)                    
+        return jsonify({
+            'success': False,
+            'message': 'What are you doing?'
+        })
+    elif request.method == 'GET':
+        if current_user.is_authenticated:
+            return redirect(url_for('index'))
+        else:
+            return render_template('login.html')
+
+@app.route('/')
+def index():
+    if current_user.is_authenticated:
+        return redirect('/admin', code=302) 
+    else:
+        return redirect(url_for('login'))
 
 @app.route('/<code>', methods=['GET'])
 def get_url(code):
