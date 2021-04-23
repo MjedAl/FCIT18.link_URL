7a5c503cdf13b3718f307aa60f55abe32a8e1afa
MjedAl <44271342+MjedAl@users.noreply.github.com>
2021-04-23 21:30:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -135,6 +135,6 @@ def not_found(error):
     }), 404
 
 port = int(os.environ.get('PORT', 5000))
-app.config['SERVER_NAME'] = 'fcit18.link:'+str(port)
+app.config['SERVER_NAME'] = 'fcit18.link'
 # app.config['SERVER_NAME'] = '127.0.0.1:'+str(port)
 app.run(host='0.0.0.0', port=port)
